7981361a876f139b4caa8ebb5371dd1e254d1528
Arno Gourdol <arno@arno.org>
2024-11-16 12:09:47 -0800

    fix

--- scripts/stage.sh
+++ scripts/stage.sh
@@ -10,7 +10,8 @@ cd "$(dirname "$0")/.."
 # Make a build
 
 ./scripts/setup.sh
-./scripts/build.sh production
+# ./scripts/build.sh production
+npm run build
 ./scripts/test.sh
 
 # Add the .nojekyll file to prevent GH Pages from processing the content
--- src/components/DocMetadata/index.js
+++ src/components/DocMetadata/index.js
@@ -1,31 +1,38 @@
 import Icon from "../Icon";
 import styles from "./styles.module.css";
+import  BrowserOnly  from "@docusaurus/BrowserOnly";
 
 export default function DocMetadata({ lastUpdated }) {
-  let version = window.MathfieldElement?.version;
   return (
-    <>
-      {lastUpdated && (
-        <p className={styles.version}>
-          <Icon name="calendar-days" />
-          Last updated {lastUpdated}
-        </p>
-      )}
-      {version && (
-        <p className={styles.version}>
-          <Icon name="code-branch" />
-          MathField <span className={styles.versionNumber}>{version}</span>
-        </p>
-      )}
-      {window.ComputeEngine && (
-        <p className={styles.version}>
-          <Icon name="code-branch" />
-          Compute Engine{" "}
-          <span className={styles.versionNumber}>
-            {window.ComputeEngine?.version}
-          </span>
-        </p>
-      )}
-    </>
+    <BrowserOnly>
+      {() => {
+        const version = window.MathfieldElement?.version;
+        return (
+          <>
+            {lastUpdated && (
+              <p className={styles.version}>
+                <Icon name="calendar-days" />
+                Last updated {lastUpdated}
+              </p>
+            )}
+            {version && (
+              <p className={styles.version}>
+                <Icon name="code-branch" />
+                MathField <span className={styles.versionNumber}>{version}</span>
+              </p>
+            )}
+            {window.ComputeEngine && (
+              <p className={styles.version}>
+                <Icon name="code-branch" />
+                Compute Engine{" "}
+                <span className={styles.versionNumber}>
+                  {window.ComputeEngine?.version}
+                </span>
+              </p>
+            )}
+          </>
+        );
+      }}
+    </BrowserOnly>
   );
 }
